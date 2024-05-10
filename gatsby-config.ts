400aadf2f76a9afb9041e1a3783b10809814bb30
yak3d <yak@yak3d.com>
2024-05-09 22:08:40 -0400

    update config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,6 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: "/starfield-library"
   plugins: [
     "gatsby-plugin-postcss",
     // "gatsby-plugin-google-gtag",
